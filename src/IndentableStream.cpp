c2f078a44417578684e50a9dc9b556fb3e44c600
RandyParedis <randy.paredis@student.uantwerpen.be>
2018-02-14 14:31:32 +0100

    Fixed some cmake issues with compiling with Visual Studio. GLTF recognized, draco not yet

--- src/IndentableStream.cpp
+++ src/IndentableStream.cpp
@@ -7,7 +7,11 @@ ostream& prefix(ostream& stream)
 	tm buf;
 	const auto now = std::chrono::system_clock::now();
 	const auto time = std::chrono::system_clock::to_time_t(now);
+#if defined(_WIN32) || defined(_WIN64)
+	localtime_s(&buf, &time);
+#else
 	localtime_r(&time, &buf);
+#endif
 	stream << "maya2glTF [" << std::put_time(&buf, "%T") << "] ";
 	return stream;
 }
@@ -38,4 +42,3 @@ std::basic_streambuf<char>::int_type IndentationBuffer::overflow(const int_type 
 
 	return traits_type::not_eof(c);
 }
-
--- src/SignalHandlers.h
+++ src/SignalHandlers.h
@@ -1,16 +1,23 @@
 #pragma once
+
+#if defined(_WIN32) || defined(_WIN64)
 #include <signal.h>
+#endif
 
-/** Overrides calls to abort to throw an exception instead of killing the process */
-class SignalHandlers
-{
-public:
-	SignalHandlers();
-	~SignalHandlers();
+	/** Overrides calls to abort to throw an exception instead of killing the process */
+	class SignalHandlers
+	{
+	public:
+		SignalHandlers();
+		~SignalHandlers();
 
-private:
-	__sighandler_t m_previousAbortSignalHandler;
-	
-	static void handleAbortSignal(int signalNumber);
-};
+	private:
+		
+#if defined(_WIN32) || defined(_WIN64)
+		_crt_signal_t m_previousAbortSignalHandler;
+#else
+		__sighandler_t m_previousAbortSignalHandler;
+#endif
 
+		static void handleAbortSignal(int signalNumber);
+	};
