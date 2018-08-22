e72dba817989df849588281660a2290a2ac31426
Peter Verswyvelen <Peter@Peters-Mac-mini.local>
2018-08-22 21:53:19 +0200

    OSX changed to CMake files, makefile is generated, but clang doesn't seem to have C++ 17's filesystem and hash_value, so not compiling yet...

--- modules/FindMaya.cmake
+++ modules/FindMaya.cmake
@@ -6,12 +6,10 @@ endif()
 set(MAYA_COMPILE_DEFINITIONS "REQUIRE_IOSTREAM;_BOOL")
 set(MAYA_INSTALL_BASE_SUFFIX "")
 set(MAYA_LIB_SUFFIX "lib")
-set(MAYA_INC_SUFFIX "include")
 
 if (WIN32)
   # Windows
   set(MAYA_INSTALL_BASE_DEFAULT "C:/Program Files/Autodesk")
-  set(OPENMAYA OpenMaya.lib)
   set(MAYA_COMPILE_DEFINITIONS "${MAYA_COMPILE_DEFINITIONS};NT_PLUGIN")
   set(MAYA_PLUGIN_EXTENSION ".mll")
 elseif(APPLE)
@@ -19,42 +17,45 @@ elseif(APPLE)
   set(MAYA_INSTALL_BASE_DEFAULT "/Applications/Autodesk")
   set(OPENMAYA libOpenMaya.dylib)
   set(MAYA_LIB_SUFFIX "Maya.app/Contents/MacOS")
-  set(MAYA_INC_SUFFIX "devkit/include")
   set(MAYA_COMPILE_DEFINITIONS "${MAYA_COMPILE_DEFINITIONS};OSMac_")
   set(MAYA_PLUGIN_EXTENSION ".bundle")
 else()
   # Linux
   set(MAYA_INSTALL_BASE_DEFAULT "/usr/autodesk")
   set(MAYA_INSTALL_BASE_SUFFIX -x64)
-  set(OPENMAYA libOpenMaya.so)
   set(MAYA_PLUGIN_EXTENSION ".so")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS" -fPIC)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
 endif()
 
 set(MAYA_INSTALL_BASE_PATH ${MAYA_INSTALL_BASE_DEFAULT} CACHE STRING "Root Maya installation path")
 set(MAYA_LOCATION ${MAYA_INSTALL_BASE_PATH}/maya${MAYA_VERSION}${MAYA_INSTALL_BASE_SUFFIX})
 
-find_path(MAYA_LIBRARY_DIR OpenMaya.lib
+# Maya include directory
+find_path(MAYA_INCLUDE_DIR maya/MFn.h
   PATHS
     ${MAYA_LOCATION}
-    $ENV{MAYA_VERSION}
+    $ENV{MAYA_LOCATION}
   PATH_SUFFIXES
-    "${MAYA_LIB_SUFFIX}/"
-  DOC "Maya library path"
-)
-
-find_path(MAYA_INCLUDE_DIR maya/Mfn.h
-  PATHS
-    ${MAYA_LOCATION}
-    $ENV{MAYA_VERSION}
-  PATH_SUFFIXES
-    "${MAYA_INC_SUFFIX}/"
+    "include/"
+    "devkit/include/"
   DOC "Maya include path"
 )
 
+set(MAYA_LIBRARIES "")
+
 set(_MAYA_LIBRARIES OpenMaya OpenMayaAnim OpenMayaFX OpenMayaRender OpenMayaUI Foundation)
+
 foreach(MAYA_LIB ${_MAYA_LIBRARIES})
-  find_library(MAYA_${MAYA_LIB}_LIBRARY NAMES ${MAYA_LIB} PATHS ${MAYA_LIBRARY_DIR} NO_DEFAULT_PATH)
+  find_library(MAYA_${MAYA_LIB}_LIBRARY
+    NAMES 
+      ${MAYA_LIB}
+    PATHS
+      ${MAYA_LOCATION}
+      $ENV{MAYA_LOCATION}
+    PATH_SUFFIXES
+      ${MAYA_LIB_SUFFIX}
+    NO_DEFAULT_PATH
+  )
   set(MAYA_LIBRARIES ${MAYA_LIBRARIES} ${MAYA_${MAYA_LIB}_LIBRARY})
 endforeach()
 
