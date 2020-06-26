1b6ccfbb6e1af064eaa73977ab7b3fb344abe608
Peter Verswyvelen <Peter@Peters-Mac-mini.local>
2020-06-26 14:03:13 +0200

    REF: Maya version can now be passed to osx project creator script

--- osx_create_project.sh
+++ osx_create_project.sh
@@ -1,1 +1,18 @@
-cmake -B "build" -G "Unix Makefiles" -D MAYA_VERSION:string=2019
+#!/bin/bash
+
+if [ $# -eq 0 ] 
+    then 
+        echo "Usage: ./osx_create_project.sh MAYA_VERSION" 
+        echo "Example: ./osx_create_project.sh 2020"
+        exit 1
+fi
+
+CMAKE="cmake"
+
+if ! [ -x "$(command -v cmake)" ]
+    then
+        echo "cmake command not found in PATH, assuming it is found at /Applications/CMake.app/Contents/bin/cmake" 
+        CMAKE="/Applications/CMake.app/Contents/bin/cmake"
+fi
+
+$CMAKE -B "build" -G "Unix Makefiles" -D MAYA_VERSION:string=$1
