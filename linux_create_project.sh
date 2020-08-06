f56a9afc445214548262176469112e2b396b49b6
Peter Verswyvelen <peter@localhost.localdomain>
2020-08-06 14:52:48 +0200

    REF: Change to get it working on Linux (Centos). Now need to update README with all the steps...

--- linux_create_project.sh
+++ linux_create_project.sh
@@ -1,1 +1,10 @@
-cmake -B "build" -G "Unix Makefiles" -D MAYA_VERSION:string=2019
+#!/bin/bash
+
+if [ $# -eq 0 ] 
+    then 
+        echo "Usage: ./linux_create_project.sh MAYA_VERSION" 
+        echo "Example: ./linux_create_project.sh 2020"
+        exit 1
+fi
+
+cmake3 -B "build" -G "Unix Makefiles" -D MAYA_VERSION:string=$1
