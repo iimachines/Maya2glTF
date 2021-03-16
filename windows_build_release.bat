a1451b3c9419e094b0ca7ba53c043a19caf0c770
Peter Verswyvelen <bugfact@gmail.com>
2021-03-16 11:55:59 +0100

    NEW: Creates ZIP file after building release

--- windows_build_release.bat
+++ windows_build_release.bat
@@ -1,3 +1,13 @@
 call windows_create_vs_project.bat %1
-call msbuild /m /p:configuration=Release build\%1\maya2glTF.sln
+if errorlevel 1 goto :fail
+
+call msbuild /m:4 /p:configuration=Release build\%1\maya2glTF.sln
+if errorlevel 1 goto :fail
+
 xcopy /s /e /y build\%1\redist\*.* build\redist\
+if errorlevel 1 goto :fail
+
+exit
+
+:fail
+echo *** FAILURE ***
--- windows_build_releases.bat
+++ windows_build_releases.bat
@@ -1,6 +1,30 @@
 @echo off
+
+setlocal 
+
 cd /d %~dp0
-start call windows_build_release 2017
-start call windows_build_release 2018
-start call windows_build_release 2019
-start call windows_build_release 2020
+
+FOR /F "tokens=*" %%g IN ('git tag -l --points-at HEAD') do (SET GIT_TAG=%%g)
+FOR /F "tokens=*" %%g IN ('git rev-parse --short HEAD') do (SET GIT_REV=%%g)
+
+set ZIP_PATH=build\maya2gltf_%GIT_TAG%_%GIT_REV%.zip
+if exist %ZIP_PATH% (del %ZIP_PATH%)
+
+echo Creating release %ZIP_PATH%
+
+start "Maya2glTF_build_2017" call windows_build_release 2017
+start "Maya2glTF_build_2018" call windows_build_release 2018
+start "Maya2glTF_build_2019" call windows_build_release 2019
+start "Maya2glTF_build_2020" call windows_build_release 2020
+
+:loop
+echo Waiting for build processes to finish...
+timeout /t 10 >nul
+tasklist /v /fi "imagename eq cmd.exe" /fo csv | findstr /i "Maya2glTF_build" >nul && goto :loop
+
+echo Creating release ZIP file...
+cmake -E tar cfv %ZIP_PATH% --format=zip build\redist
+
+:done
+
+endlocal
